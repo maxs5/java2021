450d9bf98a7df0ec030f4413377167aab81ec46c
maxs5 <93215847+maxs5@users.noreply.github.com>
2022-01-23 11:29:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int eaters = 3; // сколько людей будут есть
+
+        int water = 2500; // миллилитров воды
+        int potatoes = 4; // картофелин
+        int chicken = 3; // куриных бёдер
+        int spices = 5; // ложек специй
+
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров воды");
+        System.out.println((potatoes / eaters) + " картофелин(а)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + " ложек(ка) специй");
+
+    }
+}
